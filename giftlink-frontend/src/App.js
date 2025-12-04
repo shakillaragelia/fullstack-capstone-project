c1bd1650fff169e7b9de7414a8fa6a30131da70f
shakillaragelia <shakilla.ragelia25@gmail.com>
2025-12-04 12:11:20 -0500

    Hands-on Lab: Develop Back-end Integration for User Profile

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import Profile from './components/Profile/Profile';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  const navigate = useNavigate();
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        {/* Main gift listing page */}
+        {/* Main Pages */}
         <Route path="/" element={<MainPage />} />
         <Route path="/app" element={<MainPage />} />
 
-        {/* Login & Register pages */}
+        {/* Auth Pages */}
         <Route path="/app/login" element={<LoginPage />} />
         <Route path="/app/register" element={<RegisterPage />} />
+
+        {/* Profile Page */}
+        <Route path="/app/profile" element={<Profile />} />
       </Routes>
     </>
   );
