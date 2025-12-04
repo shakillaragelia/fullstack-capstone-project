40ce76e544c91ca8ae03088831a9d248b9870b0d
shakillaragelia <shakilla.ragelia25@gmail.com>
2025-12-04 04:08:27 -0500

    Hands-on Lab: Develop Back-end Integration for Registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -8,7 +8,7 @@ const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
@@ -21,12 +21,14 @@ connectToDatabase()
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
+// Gift API Task 1
 const giftRoutes = require('./routes/giftRoutes');
 
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+// Search API Task 1
 const searchRoutes = require('./routes/searchRoutes');
 
+// ⭐ Step 3 – Task 1: IMPORT authRoutes
+const authRoutes = require('./routes/authRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -34,12 +36,11 @@ const logger = require('./logger');
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
 
+// ⭐ Step 3 – Task 2: USE authRoutes with the /api/auth prefix
+app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
